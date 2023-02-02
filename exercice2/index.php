b65b7bb5fe64ec693f03f9fd3759a96df02a433f
KOUKOUYVES <wogelisabeth@gmail.com>
2023-02-02 17:16:45 +0000

    fresh

--- exercice2/index.php
+++ exercice2/index.php
@@ -8,51 +8,20 @@
     <title>Document</title>
 </head>
 <body><br><br>
-    <h1 style="text-align: center;">ExerciceXP-Gold 2 Motif Utilisant Des Boucles For</h1>
+    <h1 style="text-align: center;">ExerciceXP-Gold 2 Motif Utilisant Des Boucles For</h1><br>
     <div class="container">
-		<div class="form-group">
-			<form action="" method="post">
-          <div class="form-group">
-            <label for="exampleInputEmail1" class="form-label">Entrer un nombre</label>
-            <input type="number" name="values1" class="form-control" />
-          </div><br>
-
-          <div class="form-group">
-            <label for="exampleInputEmail1" class="form-label">Entrer un nombre</label>
-              <input type="number" name="values2" class="form-control" />
-          </div><br>
-				<div class="form-group">	
-					<input type="submit" name="submit" class="btn btn-primary" value="Submit" />
-				</div>	
-			</form>
-		</div>
+		
         <?php
-            if ( isset ($_POST['submit'])) {
-              $value1=$_POST['values1'];
-              $value2=$_POST['values2'];
-               
-              if($value1==""|| $value2=="")
-              {
-                echo 'Veillez renseigner tous les champs svp';
-              }else
-              {
-             
-                function multiply_two_lists($multiple1,$multiple2) {
-                  $multiple = 0;
-                  for ($i = 0; $i < strlen($multiple1); $i++) {
-                    for ($j = 0; $j < strlen($multiple2); $j++) {
-                      if ($j == $i){
-                       $multiple  = $multiple1[$i] * $multiple2[$j];
-                      echo $multiple;
-                      }
-                    }
-                   
+            function multiply_two_lists($x, $y)
+                {
+                  $a = explode(' ',trim($x));
+                  $b = explode(' ',trim($y));
+                  foreach($a as $key=>$value){
+                      $output[$key] = $a[$key]*$b[$key];
                   }
+                  return implode(' ',$output);
               }
-              multiply_two_lists(($value1),($value2));
-               
-            }
-            }
+              echo multiply_two_lists(("20 12 5"), ("1 3 3"))."\n";
 
         ?>
     </div>
